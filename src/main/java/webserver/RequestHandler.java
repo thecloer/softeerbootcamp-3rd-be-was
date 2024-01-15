ef134578477ecbb29311504d83768483688afd2e
thecloer <chltjrrb97@gmail.com>
2024-01-15 13:12:17 +0900

    refactor: request header util 클래스 RequsetHeader 클래스로 변경, method, url추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,11 +2,10 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.RequestHandlerHelper;
+import util.RequestHeader;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,8 +22,8 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // 사용자 요청 처리 부분
-            String header = RequestHandlerHelper.getHeader(in);
-            logger.debug(header);
+            RequestHeader header = new RequestHeader(in);
+            logger.debug(header.getHeader());
 
             // 사용자 요청 처리 부분 끝
             DataOutputStream dos = new DataOutputStream(out);
