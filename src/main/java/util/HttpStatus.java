69b90e244de0f3b9590108696b7da666639380f1
thecloer <chltjrrb97@gmail.com>
2024-01-16 23:52:14 +0900

    refactor: HttpStatus code 타입 String 에서 Integer로 변경

--- src/main/java/util/HttpStatus.java
+++ src/main/java/util/HttpStatus.java
@@ -4,38 +4,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum HttpStatus {
-    OK("200", "OK"),
-    CREATED("201", "Created"),
-    FOUND("302", "Found"),
-    BAD_REQUEST("400", "Bad Request"),
-    FORBIDDEN("403", "Forbidden"),
-    NOT_FOUND("404", "Not Found"),
-    INTERNAL_SERVER_ERROR("500", "Internal Server Error");
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    FOUND(302, "Found"),
+    BAD_REQUEST(400, "Bad Request"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
     private static final Map<Integer, HttpStatus> CODE_MAP = new HashMap<>();
     static {
-        CODE_MAP.put(200, OK);
-        CODE_MAP.put(201, CREATED);
-        CODE_MAP.put(302, FOUND);
-        CODE_MAP.put(400, BAD_REQUEST);
-        CODE_MAP.put(403, FORBIDDEN);
-        CODE_MAP.put(404, NOT_FOUND);
-        CODE_MAP.put(500, INTERNAL_SERVER_ERROR);
+        CODE_MAP.put(OK.code, OK);
+        CODE_MAP.put(CREATED.code, CREATED);
+        CODE_MAP.put(FOUND.code, FOUND);
+        CODE_MAP.put(BAD_REQUEST.code, BAD_REQUEST);
+        CODE_MAP.put(FORBIDDEN.code, FORBIDDEN);
+        CODE_MAP.put(NOT_FOUND.code, NOT_FOUND);
+        CODE_MAP.put(INTERNAL_SERVER_ERROR.code, INTERNAL_SERVER_ERROR);
     }
 
     public static HttpStatus getStatus(Integer code) {
         return CODE_MAP.getOrDefault(code, INTERNAL_SERVER_ERROR);
     }
 
-    private final String code;
+    private final Integer code;
     private final String message;
 
-    HttpStatus(String code, String message) {
+    HttpStatus(Integer code, String message) {
         this.code = code;
         this.message = message;
     }
 
-    public String getCode() {
+    public Integer getCode() {
         return code;
     }
 
