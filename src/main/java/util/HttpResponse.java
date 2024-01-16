911d697cfdac9dbb97615c5193fca56f447bbfda
thecloer <chltjrrb97@gmail.com>
2024-01-16 16:03:05 +0900

    chore: 안쓰이는 import 삭제

--- src/main/java/util/HttpResponse.java
+++ src/main/java/util/HttpResponse.java
@@ -7,7 +7,6 @@ import webserver.RequestHandler;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.Socket;
 
 public class HttpResponse {
 
