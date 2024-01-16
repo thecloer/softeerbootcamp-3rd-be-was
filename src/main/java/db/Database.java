2cc89c3c6cb16a9cefe6a8ccc61ddb2b2d73b747
thecloer <chltjrrb97@gmail.com>
2024-01-17 00:10:12 +0900

    refactor: Database를 static에서 concurrentHashMap으로 변경 static으로 DB를 구현하는 것 보다 ConcurrentHashMap과 DI를 이용해 구현하는 것이 테스트에 용이하고 유연해 보여 컨테이너에서 주입하는 방법으로 구현하려한다.

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -1,24 +1,22 @@
 package db;
 
-import com.google.common.collect.Maps;
-
 import model.User;
 
 import java.util.Collection;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Database {
-    private static Map<String, User> users = Maps.newHashMap();
+    private final ConcurrentHashMap<String, User> users = new ConcurrentHashMap<>();
 
-    public static void addUser(User user) {
+    public void addUser(User user) {
         users.put(user.getUserId(), user);
     }
 
-    public static User findUserById(String userId) {
+    public User findUserById(String userId) {
         return users.get(userId);
     }
 
-    public static Collection<User> findAll() {
+    public Collection<User> findAll() {
         return users.values();
     }
 }
