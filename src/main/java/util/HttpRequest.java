301930e6935f33fbf4c86bef784391c226865aac
thecloer <chltjrrb97@gmail.com>
2024-01-17 11:44:19 +0900

    refactor: HttpRequest Uri 파싱 URI 클래스 사용하지 않고 직접 파싱 URI 클래스에서 발생하는 예외에 대한 처리 문제로 URI 클래스 사용하지 않음.

--- src/main/java/util/HttpRequest.java
+++ src/main/java/util/HttpRequest.java
@@ -1,6 +1,5 @@
 package util;
 
-import java.net.URI;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,31 +7,29 @@ import java.util.StringTokenizer;
 
 public class HttpRequest {
     private final String method;
+    private final String uri;
     private final String protocol;
     private final Map<String, String> properties;
-    private final URI uri;
+
     private final String path;
-    private final String fragment;
     private final Map<String, String> queries;
 
     private HttpRequest(String method, String uri, String protocol, Map<String, String> properties) {
         this.method = method;
+        this.uri = uri;
         this.protocol = protocol;
         this.properties = properties;
 
-        URI parsedUri = URI.create(uri);
-        this.uri = parsedUri;
-        this.path = parsedUri.getPath();
-        this.fragment = parsedUri.getFragment();
+        String[] splitString = uri.split("\\?");
+        this.path = splitString[0];
 
-        String query = parsedUri.getQuery();
-        if(query == null) {
+        if(splitString.length == 1) {
             this.queries = Collections.unmodifiableMap(new HashMap<>());
             return;
         }
 
         Map<String, String> queries = new HashMap<>();
-        String[] params = query.split("&");
+        String[] params = splitString[1].split("&");
         for (String param : params) {
             StringTokenizer st = new StringTokenizer(param, "=");
             if(st.countTokens() == 2)
@@ -44,20 +41,17 @@ public class HttpRequest {
     public String getMethod() {
         return method;
     }
+    public String getUri() {
+        return uri;
+    }
     public String getProtocol() {
         return protocol;
     }
     public String getProperty(String key) {
         return properties.getOrDefault(key, null);
     }
-    public URI getUri() {
-        return uri;
-    }
     public String getPath() {
         return path;
-    }
-    public String getFragment() {
-        return fragment;
     }
     public Map<String, String> getQueries() {
         return queries;
