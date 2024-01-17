c6d70c094d09e24b190d9b41d39ebb36d4d57f33
thecloer <chltjrrb97@gmail.com>
2024-01-17 16:18:19 +0900

    refactor: HttpStatus 안쓰는 기능 삭제. 상태코드를 HttpStatus enum으로 변경하는 메서드 삭제

--- src/main/java/util/HttpStatus.java
+++ src/main/java/util/HttpStatus.java
@@ -1,8 +1,5 @@
 package util;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum HttpStatus {
     OK(200, "OK"),
     CREATED(201, "Created"),
@@ -11,21 +8,6 @@ public enum HttpStatus {
     FORBIDDEN(403, "Forbidden"),
     NOT_FOUND(404, "Not Found"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
-
-    private static final Map<Integer, HttpStatus> CODE_MAP = new HashMap<>();
-    static {
-        CODE_MAP.put(OK.code, OK);
-        CODE_MAP.put(CREATED.code, CREATED);
-        CODE_MAP.put(FOUND.code, FOUND);
-        CODE_MAP.put(BAD_REQUEST.code, BAD_REQUEST);
-        CODE_MAP.put(FORBIDDEN.code, FORBIDDEN);
-        CODE_MAP.put(NOT_FOUND.code, NOT_FOUND);
-        CODE_MAP.put(INTERNAL_SERVER_ERROR.code, INTERNAL_SERVER_ERROR);
-    }
-
-    public static HttpStatus getStatus(Integer code) {
-        return CODE_MAP.getOrDefault(code, INTERNAL_SERVER_ERROR);
-    }
 
     private final Integer code;
     private final String message;
