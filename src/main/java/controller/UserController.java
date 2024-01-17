b3c878600696c51f93bbc330b783351207e409e7
thecloer <chltjrrb97@gmail.com>
2024-01-17 11:49:06 +0900

    refactor: UserService 생성. 테스트코드 작성중 UserController에 모든 로직이 있어 테스트에 어려움을 느낌. 따라서 요청과 응답 부분을 제외한 서비스 로직을 UserService로 꺼냄. User 모델에 있던 유효성검사 UserService에서 수행

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,9 +1,7 @@
 package controller;
 
-import db.Database;
 import model.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import service.UserService;
 import util.HttpRequest;
 import util.HttpResponse;
 import util.HttpStatus;
@@ -12,30 +10,22 @@ import java.util.Map;
 
 public class UserController {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    private final UserService userService;
 
-    private final Database database;
-
-    public UserController(Database database) {
-        this.database = database;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     public void signUp(HttpRequest request, HttpResponse response) {
         try {
             Map<String, String> queries = request.getQueries();
 
-            User user = new User(
+            User user = userService.create(
                     queries.get(User.USER_ID),
                     queries.get(User.PASSWORD),
                     queries.get(User.NAME),
                     queries.get(User.EMAIL)
             );
-
-            if (database.findUserById(user.getUserId()) != null)
-                throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
-
-            database.addUser(user);
-            logger.debug("[회원가입] id: {}", user.getUserId());
 
             response.status(HttpStatus.FOUND)
                     .addHeader("Location", "/user/profile.html")
--- src/main/java/webserver/BeanContainer.java
+++ src/main/java/webserver/BeanContainer.java
@@ -3,12 +3,15 @@ package webserver;
 import controller.ResourceController;
 import controller.UserController;
 import db.Database;
+import service.UserService;
 
 public class BeanContainer {
     private static final Database database = new Database();
 
+    private static final UserService userService = new UserService(database);
+
     private static final ResourceController resourceController = new ResourceController();
-    private static final UserController userController = new UserController(database);
+    private static final UserController userController = new UserController(userService);
 
     public static ResourceController getResourceController() {
         return resourceController;
