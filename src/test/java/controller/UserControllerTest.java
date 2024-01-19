b345ef2c0b77e166e6b91cec4c7fc78dce4be68e
thecloer <chltjrrb97@gmail.com>
2024-01-19 17:03:11 +0900

    test: UserController 테스트 코드 작성
    회원가입 성공
    회원가입 실패(아이디 중복)
    회원가입 성공(입력 형식 예외: id 공백만 체크)

--- src/test/java/controller/UserControllerTest.java
+++ src/test/java/controller/UserControllerTest.java
@@ -1,15 +1,101 @@
 package controller;
 
 import db.Database;
+import model.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.http.ContentType;
+import util.http.HttpRequest;
+import util.http.HttpResponse;
+import util.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserControllerTest {
 
+    private Database database;
     private UserController userController;
 
     @BeforeEach
     public void setUp() {
-        Database database = new Database();
+        database = new Database();
         userController = new UserController(database);
+    }
+
+    @Test
+    public void 회원가입_성공() {
+        // given
+        HttpRequest request = new HttpRequest.Builder()
+                .method("GET")
+                .uri("/user/create?userId=test_id&password=test_pw&name=test_name&email=test_email")
+                .protocol("HTTP/1.1")
+                .build();
+
+        // when
+        HttpResponse response = userController.signUp(request);
+
+        // then
+        HttpResponse expected = new HttpResponse.Builder()
+                .status(HttpStatus.CREATED)
+                .addHeader("Location", "/user/profile.html?userId=test_id")
+                .build();
+
+        assertThat(response.toString())
+                .isEqualTo(expected.toString());
+    }
+
+    @Test
+    public void 회원가입_실패_중복_아이디() {
+        // given
+        User existingUser = new User.Builder()
+                .userId("test_id")
+                .password("test_pw")
+                .name("test_name")
+                .email("test_email")
+                .build();
+
+        database.addUser(existingUser);
+
+        HttpRequest request = new HttpRequest.Builder()
+                .method("GET")
+                .uri("/user/create?userId=test_id&password=test_pw2&name=test_name2&email=test_email2")
+                .protocol("HTTP/1.1")
+                .build();
+
+        // when
+        HttpResponse response = userController.signUp(request);
+
+        // then
+        HttpResponse expected = new HttpResponse.Builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(ContentType.JSON)
+                .body("{\"message\":\"이미 존재하는 아이디입니다.\"}")
+                .build();
+
+        assertThat(response.toString())
+                .isEqualTo(expected.toString());
+    }
+
+    @Test
+    public void 회원가입_실패_입력_형식_예외() {
+        // given
+        HttpRequest request = new HttpRequest.Builder()
+                .method("GET")
+                .uri("/user/create?userId===&password=test_pw2&name=test_name2&email=test_email2&==&=&====&")
+                .protocol("HTTP/1.1")
+                .build();
+
+        // when
+        HttpResponse response = userController.signUp(request);
+
+        // then
+        HttpResponse expected = new HttpResponse.Builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(ContentType.JSON)
+                .body("{\"message\":\"userId가 입력되지 않았습니다.\"}")
+                .build();
+
+        assertThat(response.toString())
+                .isEqualTo(expected.toString());
     }
 }
