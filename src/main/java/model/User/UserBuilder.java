887fd7fcbd1b79aa88e17217143bf4b17814af9d
thecloer <chltjrrb97@gmail.com>
2024-01-23 14:35:53 +0900

    refactor: UserBuilder에서 json으로 모델 객체를 생성하는 부분 추출 완료

--- src/main/java/model/User/UserBuilder.java
+++ src/main/java/model/User/UserBuilder.java
@@ -1,10 +1,6 @@
 package model.User;
 
-import annotation.Column;
-import util.JSON;
-
-import java.lang.reflect.Field;
-import java.util.Map;
+import util.ModelConverter;
 
 public class UserBuilder {
     private String userId;
@@ -13,34 +9,7 @@ public class UserBuilder {
     private String email;
 
     static public User fromStringifiedJson(String stringifiedJson) {
-        Map<String, String> keyValueMap = JSON.parse(stringifiedJson);
-
-        User user = new User("", "", "", "");
-        // TODO: 리플렉션으로 어노테이션 맴버에 매핑하는 부분 추출
-        Class<User> userClass = User.class;
-        for (Field field : userClass.getDeclaredFields()) {
-            if (!field.isAnnotationPresent(Column.class)) {
-                continue;
-            }
-
-            Column column = field.getAnnotation(Column.class);
-            String key = column.value();
-            if (!keyValueMap.containsKey(key)) {
-                continue;
-            }
-
-            String value = keyValueMap.get(key);
-
-            try {
-                field.setAccessible(true);
-                field.set(user, value);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-                throw new RuntimeException("User 객체 생성에 실패했습니다. (" + e.getMessage() + ")");
-            }
-        }
-
-        return user;
+        return ModelConverter.convert(stringifiedJson, User.class);
     }
 
     public UserBuilder userId(String userId) {
