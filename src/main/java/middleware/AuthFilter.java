0c928de22fd1b9f654f02f261e43c2cda9f0bc76
thecloer <chltjrrb97@gmail.com>
2024-01-25 12:19:00 +0900

    feat: AuthFilter 미들웨어 생성
    로그인 상태 때만 접근 가능한 자원과 로그아웃 상태일 때만 접근 가능한 자원을 분리하여 쿠키-세션을 통해 접근 제한
    접근 가능할 경우 세션을 요청 객체에 넣어 이후 미들웨어로 전달 (전달은 RequestPipeline 에서 진행)

---
+++ src/main/java/middleware/AuthFilter.java
@@ -0,0 +1,74 @@
+package middleware;
+
+import exception.RedirectException;
+import session.Session;
+import session.SessionManager;
+import util.http.HttpRequest;
+import webserver.Router;
+
+import java.util.Set;
+
+public class AuthFilter implements Middleware {
+
+    private static final Set<String> AUTHENTICATED = Set.of(
+            "GET /user/profile.html"
+    );
+    private static final Set<String> UNAUTHENTICATED = Set.of(
+            "GET /user/login.html",
+            "GET /user/form.html"
+    );
+
+    @Override
+    public HttpRequest process(HttpRequest request) {
+        String routeKey = Router.getRouteKey(request);
+        Session session = extractSession(request);
+
+        session = checkAndRenewSession(session);
+
+        if (AUTHENTICATED.contains(routeKey) && session == null)
+            throw new RedirectException("/user/login.html");
+
+        if (UNAUTHENTICATED.contains(routeKey) && session != null)
+            throw new RedirectException("/");
+
+        request.setSession(session);
+
+        return request;
+    }
+
+    private static Session extractSession(HttpRequest request) {
+        String cookie = request.getProperty("cookie");
+        String sessionId = extractCookie(cookie, SessionManager.COOKIE_KEY);
+        return SessionManager.getSession(sessionId);
+    }
+
+    private static String extractCookie(String cookieString, String key) {
+        if(cookieString == null)
+            return "";
+
+        String prefix = key + "=";
+        int startIndex = cookieString.indexOf(prefix);
+        if (startIndex == -1)
+            return "";
+        startIndex += prefix.length();
+
+        int endIndex = cookieString.indexOf(";", startIndex);
+        if (endIndex == -1)
+            endIndex = cookieString.length();
+
+        return cookieString.substring(startIndex, endIndex);
+    }
+
+    private static Session checkAndRenewSession(Session session) {
+        if (session == null)
+            return null;
+
+        if (session.isExpired()) {
+            SessionManager.distroySession(session.getSessionId());
+            return null;
+        }
+
+        session.renew();
+        return session;
+    }
+}
