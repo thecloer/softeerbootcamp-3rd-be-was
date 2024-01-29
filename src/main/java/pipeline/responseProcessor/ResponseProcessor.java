1762bf8dfd7b4a2a7d67f76e98d3a31f180399a2
thecloer <chltjrrb97@gmail.com>
2024-01-29 17:35:40 +0900

    fix: 오타 수정

--- src/main/java/pipeline/responseProcessor/ResponseProcessor.java
+++ src/main/java/pipeline/responseProcessor/ResponseProcessor.java
@@ -3,5 +3,5 @@ package pipeline.responseProcessor;
 import util.http.HttpResponse;
 
 public interface ResponseProcessor {
-    HttpResponse process(HttpResponse request);
+    HttpResponse process(HttpResponse response);
 }
